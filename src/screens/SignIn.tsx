d6b09caf4e348f87b245016753a87dc0679816d1
Pablo Silva <pablolucio_@hotmail.com>
2023-11-14 06:42:35 -0300

    styles: created Button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,5 +1,6 @@
 import { Container, Slogan, Title } from "./styles";
 import backgroundImg from '../assets/background.png'
+import { Button } from "../components/Button";
 
 export function SignIn() {
     return (
@@ -8,6 +9,7 @@ export function SignIn() {
             <Slogan>
                 Gestão de uso de veículos
             </Slogan>
+            <Button title='Entrar com Google' />
         </Container>
     )
 }
