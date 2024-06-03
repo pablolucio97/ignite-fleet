40dd5d06d7d0b34c28ab23bb0d9b8cd263a5d6d5
Pablo <pablolucio_@hotmail.com>
2024-06-03 09:10:14 -0300

    chore: configures google sign authentication

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,15 +1,40 @@
-import { Container, Slogan, Title } from "./styles";
-import backgroundImg from '../assets/background.png'
+import { IOS_CLIENT_ID, WEB_CLIENT_ID } from "@env";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import { useState } from "react";
+import backgroundImg from "../assets/background.png";
 import { Button } from "../components/Button";
+import { Container, Slogan, Title } from "./styles";
 
 export function SignIn() {
-    return (
-        <Container source={backgroundImg}>
-            <Title>Ignite Fleet</Title>
-            <Slogan>
-                Gestão de uso de veículos
-            </Slogan>
-            <Button title='Entrar com Google' />
-        </Container>
-    )
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  GoogleSignin.configure({
+    scopes: ["email", "profile"],
+    webClientId: WEB_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+  });
+
+  const handleSign = async () => {
+    try {
+      setIsAuthenticating(true);
+      const authInfo = await GoogleSignin.signIn();
+      console.log(authInfo)
+    } catch (error) {
+      console.log("Error at trying to sign in: ", error);
+    } finally {
+      setIsAuthenticating(false);
+    }
+  };
+
+  return (
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button
+        title="Entrar com Google"
+        onPress={handleSign}
+        isLoading={isAuthenticating}
+      />
+    </Container>
+  );
 }
