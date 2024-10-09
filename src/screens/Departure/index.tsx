3bdd9aafc8e2326eebf9231f18e13310e61ce56a
Pablo <pablolucio_@hotmail.com>
2024-10-09 08:31:38 -0300

    feat: handles keyboardview behavior on Departure screen

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,10 @@
 import { useRef } from "react";
-import { TextInput } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+} from "react-native";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlate";
@@ -7,29 +12,40 @@ import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
 export function Departure() {
   const descriptionRefInput = useRef<TextInput>(null);
+  const keyboardAvoidingViewBehavior =
+    Platform.OS === "android" ? "height" : "position";
+
   function handleDepartureRegister() {
     console.log("OK!");
   }
+
   return (
     <Container>
       <Header title="Saída" />
-      <Content>
-        <LicensePlateInput
-          label="Placa do veículo"
-          placeholder="BRA1234"
-          onSubmitEditing={() => descriptionRefInput.current?.focus()}
-          returnKeyType="next"
-        />
-        <TextAreaInput
-          ref={descriptionRefInput}
-          label="Finalizade"
-          placeholder="Vou utilizar o veículo para..."
-          onSubmitEditing={handleDepartureRegister}
-          returnKeyType="send"
-          blurOnSubmit
-        />
-        <Button title="Registar Saída" />
-      </Content>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={keyboardAvoidingViewBehavior}
+      >
+        <ScrollView>
+          <Content>
+            <LicensePlateInput
+              label="Placa do veículo"
+              placeholder="BRA1234"
+              onSubmitEditing={() => descriptionRefInput.current?.focus()}
+              returnKeyType="next"
+            />
+            <TextAreaInput
+              ref={descriptionRefInput}
+              label="Finalizade"
+              placeholder="Vou utilizar o veículo para..."
+              onSubmitEditing={handleDepartureRegister}
+              returnKeyType="send"
+              blurOnSubmit
+            />
+            <Button title="Registar Saída" />
+          </Content>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </Container>
   );
 }
