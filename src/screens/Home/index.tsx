492f391c4c25b1e6f0cb33813313b8ce5f5202f0
Pablo <pablolucio_@hotmail.com>
2024-10-09 07:51:46 -0300

    feat: creates CarStatus component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { CarStatus } from "../../components/CarStatus";
 import { HomeHeader } from "./components/Header";
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 export function Home() {
   return (
     <Container>
       <HomeHeader />
+      <Content>
+        <CarStatus licensePlate="XXX-1234" />
+      </Content>
     </Container>
   );
 }
