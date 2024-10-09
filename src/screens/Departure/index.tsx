b5a6fc7c1d3d4b3119520b16d8f6722e99e28b27
Pablo <pablolucio_@hotmail.com>
2024-10-09 08:08:21 -0300

    feat: creates TextAreaInput component

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { LicensePlateInput } from "../../components/LicensePlate";
+import { TextAreaInput } from "../../components/TextAreaInput";
 import { Container, Content } from "./styles";
 export function Departure() {
   return (
@@ -7,6 +8,10 @@ export function Departure() {
       <Header title="Saída" />
       <Content>
         <LicensePlateInput label="Placa do veículo" placeholder="BRA1234" />
+        <TextAreaInput
+          label="Finalizade"
+          placeholder="Vou utilizar o veículo para..."
+        />
       </Content>
     </Container>
   );
